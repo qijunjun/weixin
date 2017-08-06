239330204ae7fba192ba71b3266c49dea05f1f0e
qijunjun <2269873327@qq.com>
2017-08-06 22:54:09 +0800

    [新增]搜索页面、列表页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,7 @@
 //index.js
 //获取应用实例
 var app = getApp()
+//page用来创建页面对象
 Page({
   data: {
     motto: 'Hello World',
@@ -13,6 +14,11 @@ Page({
     })
   },
   onLoad: function () {
+    //加载数据并呈现在页面上
+    //this.data.message = xxx 不可以,因为小程序平台捕获不到,可以使用下面这张方式
+    //this.setData({
+    //  message:Data.now()
+    //})
     console.log('onLoad')
     var that = this
     //调用应用实例的方法获取全局数据
@@ -21,6 +27,20 @@ Page({
       that.setData({
         userInfo:userInfo
       })
+    })
+    //获取一个API中的数据，不要使用Ajax API因为AJAX的API是BOM
+    //使用微信官方API
+    wx.request({
+      url:'https"//api.douban.com/v2/move/in_theaters',
+      data:{},
+      header:{
+        'Content-Type':'application/json'
+      },
+      success:function(res){
+        that.setData({
+          list:res.data.subjects
+        })
+      }
     })
   }
 })
